dd9e4f829f52e4745f398afd5b6ff6f6c6cd5fb6
aulia <rahmanaulia336@gmail.com>
2025-01-20 10:21:20 +0700

    .

--- resources/views/log_scale/index.blade.php
+++ resources/views/log_scale/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
 @section('content')
-    @include('layouts.navbars.auth.topnav', ['title' => 'Log Weight'])
+    @include('layouts.navbars.auth.topnav', ['title' => 'Log Scale'])
 
     <div class="row mt-1 px-1">
         <div class="card">
